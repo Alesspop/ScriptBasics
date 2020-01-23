3308b487b5b05a5bd5b853598d4446293f06f166
Alesspop <46914352+Alesspop@users.noreply.github.com>
2020-01-22 19:48:43 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// console.log
+console.log("It's working!");
+
+// Alert
+window.alert("This is a Alessandra's howework assignment!");
+
+// Prompt, document.write
+var name = window.prompt( "What is your name?");
+document.write("Hi " + name + " ! Nice to meet you. I am Alessandra.");
+// Operators, numerical value
+var a = 11;
+var b = 4;
+var theNumbersMatch;
+
+if ( a == b) {
+    theNumbersMatch = true
+} else {
+    theNumbersMatch = false
+}
+
+document.write("The numbers match:" + theNumbersMatch);
+
+// String Value
+var x = "purple";
+var z = "yellow";
+document.write("I think I like " + x + " more than " + z + ".");
+
+// If statement
+var fruit = "bananas";
+var color = "pink";
+
+if( (fruit == "bananas") && ( color == "yellow")) {
+    document.write( " Bananas are yellow.");
+} else {
+    document.write( "Bananas are not that color, they are yellow.");
+}
+
+tester
+
+
+
+
+
